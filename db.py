1dda3f5746459fe9270d262f4745dc4679a574f2
Ramon181 <ramonantonio29970476@gmail.com>
2023-07-06 17:29:35 -0500

    f

--- db.py
+++ db.py
@@ -1,19 +1,16 @@
 import mysql.connector
-from dotenv import load_dotenv
 from models.teams import teams
 from models.league import leagues
 from models.positions import positions
-from os import environ
 
-load_dotenv()
 
 
 def db():
     data = mysql.connector.connect(
-        host=environ.get("DB_HOST"),
-        user=environ.get("DB_USER"),
-        password=environ.get("DB_PASS"),
-        database=environ.get("DB_NAME"),
+        host="localhost",
+        user="root",
+        password="12345",
+        database="tablas_db",
     )
     return data
 
